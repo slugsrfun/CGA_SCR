7cd29c7bf1df56a462171b58f48441cb6939feca
John Robinson <robinson.johnd@gmail.com>
2026-08-13 11:57:45 -0400

    reboot

--- scr/render_fitHooker_bothSexes.R
+++ scr/render_fitHooker_bothSexes.R
@@ -1,6 +1,6 @@
 library(rmarkdown)
 
-setwd("/scratch/jdrobins/CGA_BlackBears/scr/")
+setwd("/scratch/jdrobins/CGA_SCR/scr/")
 
-render("13_fitHooker_bothSexes.Rmd", output_dir = "/scratch/jdrobins/CGA_BlackBears/scr/")
+render("13_fitHooker_bothSexes.Rmd", output_dir = "/scratch/jdrobins/CGA_SCR/scr/")
 
