ed0d20218f9d457d389d04efbecdb4876bbd8747
John Robinson <robinson.johnd@gmail.com>
2026-09-08 16:21:09 -0400

    update files for 13_fitHooker_bothSexes

---
+++ scr/fitHooker_bothSexes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=fit_both
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64gb
+#SBATCH --time=60:00:00
+#SBATCH --output=/scratch/jdrobins/CGA_SCR/scr/fitHooker_bothSexes.o
+#SBATCH --error=/scratch/jdrobins/CGA_SCR/scr/fitHooker_bothSexes.o
+
+ml Miniforge3/24.11.3-0
+
+source activate /home/jdrobins/veGAbears
+
+cd /scratch/jdrobins/CGA_SCR/scr
+
+Rscript render_fitHooker_bothSexes.R
+
+conda deactivate 
+
+cp 13_fitHooker_bothSexes.pdf /home/jdrobins/CGA_SCR/PDFs/
+cp samples-bothSexes.gzip /home/jdrobins/CGA_SCR/scr/
+
+scontrol show job ${SLURM_JOB_ID}
