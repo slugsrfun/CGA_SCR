a14db2756bb9729e3030b4935476d1f4a03b07af
John Robinson <robinson.johnd@gmail.com>
2026-09-08 15:40:27 -0400

    clean up

--- other/render_assessPVA.R
+++
@@ -1,6 +0,0 @@
-library(rmarkdown)
-
-setwd("/scratch/jdrobins/CGA_SCR/other/")
-
-render("14_assessPVA.Rmd", output_dir = "/scratch/jdrobins/CGA_SCR/other/")
-
--- scr/render_fitHooker_bothSexes.R
+++
@@ -1,6 +0,0 @@
-library(rmarkdown)
-
-setwd("/scratch/jdrobins/CGA_SCR/scr/")
-
-render("13_fitHooker_bothSexes.Rmd", output_dir = "/scratch/jdrobins/CGA_SCR/scr/")
-
